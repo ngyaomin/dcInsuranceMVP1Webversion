7d0b181569c7f77e4486a68096640df67900dc62
Yao Min <ngyaominsg@gmail.com>
2020-08-12 23:09:48 +0800

    setup body parser

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
