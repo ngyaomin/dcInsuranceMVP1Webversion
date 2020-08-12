8886577748e2f62a26aa80f9f095c1f1fdd67a11
Yao Min <ngyaominsg@gmail.com>
2020-08-12 22:51:23 +0800

    set up mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var port = 3000;
+var mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
@@ -9,3 +10,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/requestInsurance");
+
